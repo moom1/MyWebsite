9eceb570ab40ccbeeb2861cde3f036f36b82defa
moom1 <aayyash9@gmail.com>
2020-08-10 02:09:48 +0800

    test db

--- test.php
+++ test.php
@@ -1,13 +1,14 @@
 <?php
 
 
-require_once('connection.php');
-require_once('dbfunctions.php');
+require_once('database/connection.php');
+require_once('database/dbfunctions.php');
 
 $test = read("test");
 
 
 
 echo $test;
+echo "test";
 
 ?>
