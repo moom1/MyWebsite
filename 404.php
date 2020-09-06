b6ce719ca6754a11bb20f02cac9ab9cd33a0420d
moom1 <aayyash9@gmail.com>
2020-09-06 16:21:02 +0800

    error page

---
+++ 404.php
@@ -0,0 +1,72 @@
+<?php $page = "projects";?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <?php include "head.php"?>
+        <title>About</title>
+    </head>
+
+    <style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bgimg {
+  background: url('https://source.unsplash.com/J3cXXMsNsjw');
+  height: 130%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: #FFF;
+}
+
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 16px;
+}
+
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+}
+
+.middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+hr {
+  margin: auto;
+  width: 40%;
+}
+</style>
+
+    <!-- Navigation -->
+    
+    <?php include "topNav.php"?>
+
+    <body>
+
+    <div class="bgimg">
+    <div class="topleft">
+    </div>
+    <div class="middle">
+        <h1>Page doesn't exist</h1>
+        <hr>
+    </div>
+    <div class="bottomleft">
+    </div>
+    </div>
+
+</body>
+
+
+        
+</html>
