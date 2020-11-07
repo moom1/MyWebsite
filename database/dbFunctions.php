ca42b61a8e9d4e210662a0dfff0caae1cabef808
moom1 <aayyash9@gmail.com>
2020-11-07 19:29:58 +0300

    db func  2.3

--- database/dbFunctions.php
+++ database/dbFunctions.php
@@ -1,5 +1,5 @@
 <?php
-require_once('connect.php');
+require('connect.php');
 
 //sets connections
 function setConnectionInfo($values=array()){
--- projects.php
+++ projects.php
@@ -1,6 +1,4 @@
 <?php 
-
-
 $page = "projects";
 require_once('database/connect.php');
 require_once('database/dbFunctions.php');
@@ -11,82 +9,5 @@ foreach ($tests as $test) {
   echo "inside tests";
 }
 
+testF("TEST FUNCTION");
 ?>
-
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <?php include "head.php"?>
-        <title>Projects</title>
-    </head>
-
-    <style>
-body, html {
-  height: 100%;
-  margin: 0;
-}
-
-.bgimg {
-  background: url('https://source.unsplash.com/J3cXXMsNsjw');
-  height: 130%;
-  background-position: center;
-  background-size: cover;
-  position: relative;
-  color: #FFF;
-}
-
-.topleft {
-  position: absolute;
-  top: 0;
-  left: 16px;
-}
-
-.bottomleft {
-  position: absolute;
-  bottom: 0;
-  left: 16px;
-}
-
-.middle {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
-
-hr {
-  margin: auto;
-  width: 40%;
-}
-</style>
-
-    <!-- Navigation -->
-    
-    <?php include "topNav.php"?>
-
-    <body>
-
-    <div class="bgimg">
-    <div class="topleft">
-    </div>
-    <div class="middle">
-        <h1>
-        <?php
-        
-       
-        echo "Hi";
-        Under construction
-        ?>
-        </h1>
-        <hr>
-    </div>
-    <div class="bottomleft">
-    </div>
-    </div>
-
-</body>
-
-
-        
-</html>
