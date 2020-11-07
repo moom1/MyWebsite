afc90629bcf5490f1c46dbbd3ccbb2a6ff094978
moom1 <aayyash9@gmail.com>
2020-11-07 18:36:17 +0300

    db 2.2

--- database/connect.php
+++ database/connect.php
@@ -1,19 +1,23 @@
 <?php
-    $db_host = "localhost";
+   /*  $db_host = "localhost";
     $db_name = "abdulrb0_portfolio";
     $db_user = "abdulrb0_root";
-    $db_pass = "7q26h8P86eC6acB";
+    $db_pass = "7q26h8P86eC6acB"; */
 
 
-   /*  define('DBCONNECTION', 'mysql:host=localhost;dbname=abdulrb0_portfolio');
+    define('DBCONNECTION', 'mysql:host=localhost;dbname=abdulrb0_portfolio');
     define('DBUSER','abdulrb0_root');
-    define('DBPASS','7q26h8P86eC6acB'); */
+    define('DBPASS','7q26h8P86eC6acB');
 
 
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $db = new PDO(DBCONNECTION, DBUSER, DBPASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "hi";
+
+   /*  $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
+
+    echo "using constants";
     $sql = "SELECT * FROM test" ;
 	try {
 	$statement = $db->prepare($sql);
