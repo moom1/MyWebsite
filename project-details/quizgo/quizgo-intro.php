31b821560e812e67689045104f79a24bd319f9dd
moom1 <aayyash9@gmail.com>
2020-11-08 02:03:36 +0300

    details 1

--- project-details/quizgo/quizgo-intro.php
+++ project-details/quizgo/quizgo-intro.php
@@ -13,8 +13,6 @@ $page = "projects";
     <!-- Navigation -->
     
     <?php include "/home2/abdulrb0/public_html/portfolio/topNav.php"?>
-   
-
 
     <div class="container p-3 text-center" style="background-image: url('/portfolio/resources/pictures/blog-bg.png'); background-size: cover; background-repeat: no-repeat; background-position: center;">
         <div  class="col-md-5 p-lg-5 mx-auto my-5">
@@ -22,5 +20,55 @@ $page = "projects";
         <p class="lead font-weight-normal text-white">QuizGo is a platform built to facilitate MCQ testing in an educational environment. It is Built to serve lecturers, advisors, and most importantly, students. <a href="/portfolio/resources/documents/QuizGo-report.pdf">Techical report and Documentation </a></p>
         </div>
     </div>
+
+
+    <div class="container">
+  <div class="card-deck mb-3 text-center">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Free</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>10 users included</li>
+          <li>2 GB of storage</li>
+          <li>Email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Pro</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>20 users included</li>
+          <li>10 GB of storage</li>
+          <li>Priority email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Enterprise</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>30 users included</li>
+          <li>15 GB of storage</li>
+          <li>Phone and email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+      </div>
+    </div>
+  </div>
         
 </html>
