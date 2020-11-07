7dc405b3a8db7e9fd2a57f937e5b530701060898
moom1 <aayyash9@gmail.com>
2020-11-07 23:57:51 +0300

    qg in 2.6

--- project-details/quizgo/quizgo-intro.php
+++ project-details/quizgo/quizgo-intro.php
@@ -3,20 +3,20 @@
 <!DOCTYPE html>
     <html lang="en">
     <head>
-        <?php include "./head.php"?>
+        <?php include "/portfolio/head.php"?>
         <title>Projects - QuizGo</title>
     </head>
 
     <!-- Navigation -->
     
-    <?php include "./topNav.php"?>
+    <?php include "/porfolio/topNav.php"?>
    
 
 
-    <div class="container p-3 text-center" style="background-image: url('resources/pictures/blog-bg.png'); background-size: cover; background-repeat: no-repeat; background-position: center;">
+    <div class="container p-3 text-center" style="background-image: url('/portfolio/resources/pictures/blog-bg.png'); background-size: cover; background-repeat: no-repeat; background-position: center;">
         <div  class="col-md-5 p-lg-5 mx-auto my-5">
         <h1 class="display-4 font-weight-normal text-white">QuizGo</h1>
-        <p class="lead font-weight-normal text-white">QuizGo is a platform built to facilitate MCQ testing in an educational environment. It is Built to serve lecturers, advisors, and most importantly, students. <a href="../../resources/documents/QuizGo-report.pdf">Techical report and Documentation </a></p>
+        <p class="lead font-weight-normal text-white">QuizGo is a platform built to facilitate MCQ testing in an educational environment. It is Built to serve lecturers, advisors, and most importantly, students. <a href="/portfolio/resources/documents/QuizGo-report.pdf">Techical report and Documentation </a></p>
         </div>
     </div>
         
